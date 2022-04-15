3f57a3c75ef1b6285731587c783d8153160142f1
Alexander-Linnik <Aliaksandr.Linnik@itechart-group.com>
2022-04-15 14:04:52 +0300

    Define data access layer (Repository defined)

---
+++ ITechArt.UniversityApp.Core.DataAccess.Abstracts/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITechArt.UniversityApp.Core.DataAccess
+{
+	public interface IRepository<T>
+	{
+		IEnumerable<T> GetAll();
+		T Get(int id);
+		void Create(T TObject);
+		void Update(T TObject);
+		void Delete(int id);
+		void Save();
+
+	}
+}
---
+++ ITechArt.UniversityApp.Core.DataAccess.Abstracts/ITransaction.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITechArt.UniversityApp.Core.DataAccess
+{
+	internal interface ITransaction
+	{
+	}
+}
